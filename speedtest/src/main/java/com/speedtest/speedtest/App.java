ec366c71b3a9579782518dcb477c79fbc20e5f41
Hugo Morales Arjón <hugo_morarj@hotmail.com>
2017-08-16 15:28:16 -0500

    Agrega proyecto.

---
+++ speedtest/src/main/java/com/speedtest/speedtest/App.java
@@ -0,0 +1,13 @@
+package com.speedtest.speedtest;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
