428b56f73f3eca1a12d0b5e05d8f2a0dc9b7b3a7
Hugo Morales Arjón <hugo_morarj@hotmail.com>
2017-08-17 18:40:01 -0500

    Adds console Logger.

--- speedtest/src/test/java/com/speedtest/net/speedtest/ConnectionSpeedTest.java
+++ speedtest/src/test/java/com/speedtest/net/speedtest/ConnectionSpeedTest.java
@@ -10,46 +10,46 @@ import org.testng.annotations.BeforeClass;
 
 import java.util.concurrent.TimeUnit;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.junit.Assert;
 import org.openqa.selenium.WebElement;
+import org.testng.Reporter;
 import org.testng.annotations.AfterClass;
 
 public class ConnectionSpeedTest extends TestSuiteBase {
+	private static final Logger log = LogManager.getLogger(ConnectionSpeedTest.class.getName());
+	
 	@Test
 	public void connectionSpeedTest() {
+		// Click on the Start Button to start the test
 		home.clickStartButton();
-		WebElement result = home.getPingSpeed();
-		if(result != null) {
-			System.out.println(result.getText());
+		
+		// Get the Ping Speed Result
+		WebElement pingSpeedResult = home.getPingSpeed();
+		try {
+			Assert.assertNotEquals(null, pingSpeedResult);
+			log.info("Ping: " + pingSpeedResult.getText());
+		} catch (Exception e) {
+			log.info("Couldnt get ping speed.");
 		}
-		result = home.getDownloadSpeed();
-		if(result != null) {
-			System.out.println(result.getText());
+		
+		// Get the Download Speed Result
+		WebElement downloadSpeedResult = home.getDownloadSpeed();
+		try {
+			Assert.assertNotEquals(null, downloadSpeedResult);
+			log.info("Download: " + downloadSpeedResult.getText());
+		} catch (Exception e) {
+			log.info("Couldnt get download speed.");
 		}
-		result = home.getUploadSpeed();
-		if(result != null) {
-			System.out.println(result.getText());
+		
+		// Get the Upload Speed Result
+		WebElement uploadSpeedResult = home.getUploadSpeed();
+		try {
+			Assert.assertNotEquals(null, uploadSpeedResult);
+			log.info("Upload: " + uploadSpeedResult.getText());
+		} catch (Exception e) {
+			log.info("Couldnt get upload speed.");
 		}
-	}
-
-	@BeforeMethod
-	public void beforeMethod() {
-		// Maximize the browser's window
-		// driver.manage().window().maximize();
-		driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
-		// Open the Application
-		driver.get(baseUrl);
-	}
-
-	@AfterMethod
-	public void afterMethod() {
-		driver.close();
-	}
-
-	@BeforeClass
-	public void beforeClass() {
-	}
-
-	@AfterClass
-	public void afterClass() {
 	}
 }
--- speedtest/src/test/java/pageObjectModels/TestSuiteBase.java
+++ speedtest/src/test/java/pageObjectModels/TestSuiteBase.java
@@ -2,6 +2,8 @@ package pageObjectModels;
 
 import java.net.MalformedURLException;
 import java.net.URL;
+import java.util.concurrent.TimeUnit;
+
 import org.openqa.selenium.Platform;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
@@ -10,22 +12,27 @@ import org.openqa.selenium.remote.DesiredCapabilities;
 import org.openqa.selenium.remote.RemoteWebDriver;
 import org.openqa.selenium.support.PageFactory;
 import org.testng.annotations.AfterClass;
+import org.testng.annotations.AfterSuite;
 import org.testng.annotations.BeforeClass;
+import org.testng.annotations.BeforeSuite;
 import org.testng.annotations.Parameters;
 
 public class TestSuiteBase {
 	protected WebDriver driver;
 	protected HomeFactory home;
-	protected String baseUrl;
 
 	@Parameters({ "platform", "browser", "version", "url", "mode" })
 	@BeforeClass(alwaysRun = true)
 	public void setup(String platform, String browser, String version, String url, String mode)
 			throws MalformedURLException {
-		baseUrl = url;
-		
 		driver = getDriverInstance(platform, browser, version, url, mode);
 		home = PageFactory.initElements(driver, HomeFactory.class);
+
+		// Maximize the browser's window
+		// driver.manage().window().maximize();
+		driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+		// Open the Application
+		driver.get(url);
 	}
 
 	public static WebDriver getDriverInstance(String platform, String browser, String version, String url, String mode)
@@ -57,21 +64,22 @@ public class TestSuiteBase {
 		} else if (mode.equalsIgnoreCase("local")) {
 			// Browsers
 			if (browser.equalsIgnoreCase("firefox")) {
-				System.setProperty("webdriver.gecko.driver", "C:\\Users\\HugoMorArj\\OneDrive\\Cursos\\Udemy\\Selenium-Webdriver-with-Java\\drivers\\geckodriver.exe");
+				System.setProperty("webdriver.gecko.driver",
+						"C:\\Users\\HugoMorArj\\OneDrive\\Cursos\\Udemy\\Selenium-Webdriver-with-Java\\drivers\\geckodriver.exe");
 				driver = new FirefoxDriver();
 			}
 			if (browser.equalsIgnoreCase("chrome")) {
-				System.setProperty("webdriver.chrome.driver", "C:\\Users\\HugoMorArj\\OneDrive\\Cursos\\Udemy\\Selenium-Webdriver-with-Java\\drivers\\chromedriver.exe");
+				System.setProperty("webdriver.chrome.driver",
+						"C:\\Users\\HugoMorArj\\OneDrive\\Cursos\\Udemy\\Selenium-Webdriver-with-Java\\drivers\\chromedriver.exe");
 				driver = new ChromeDriver();
 			}
 		}
-		
+
 		return driver;
 	}
 
 	@AfterClass(alwaysRun = true)
 	public void afterClass() {
-		driver.close();
 		driver.quit();
 	}
 }
